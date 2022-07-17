c887e37304b079cb45a25cfa2f27d6c60f631cfd
optimalstrategy <17456182+OptimalStrategy@users.noreply.github.com>
2022-07-17 00:22:08 -0400

    feat: add another test future

--- experiments/src/main.rs
+++ experiments/src/main.rs
@@ -1,3 +1,4 @@
+use std::cell::Cell;
 use std::time::Duration;
 
 use experiments::piggyback_scheduler::{AsyncFnResult, FnResult, Function, Object, OpCode, Vm};
@@ -17,6 +18,54 @@ fn sleep_test(id: usize, millis: u64) -> AsyncFnResult {
     tokio::time::sleep(Duration::from_millis(millis)).await;
     Ok(Object::Str(format!("Result of fn{id} after {millis}ms")))
   })
+}
+
+fn poll_n_test(n: usize) -> AsyncFnResult {
+  println!("(debug) scheduling poll-n-times with n={n}");
+  Box::new(poll_n::PollNFuture::new(n))
+}
+
+pub mod poll_n {
+  use std::task::Poll;
+
+  use super::*;
+
+  pub struct PollNFuture {
+    n: usize,
+    so_far: Cell<usize>,
+  }
+  impl PollNFuture {
+    pub fn new(n: usize) -> Self {
+      Self {
+        n,
+        so_far: Cell::new(0),
+      }
+    }
+  }
+  impl std::future::Future for PollNFuture {
+    type Output = FnResult;
+
+    fn poll(
+      self: std::pin::Pin<&mut Self>,
+      cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+      println!(
+        "(debug)     Polling poll-n-times with n={}, {} so far ",
+        self.n,
+        self.so_far.get()
+      );
+      if self.so_far.get() >= self.n {
+        Poll::Ready(Ok(Object::Str(format!(
+          "Finished after being polled {} times!",
+          self.n
+        ))))
+      } else {
+        self.so_far.set(self.so_far.get() + 1);
+        cx.waker().clone().wake();
+        Poll::Pending
+      }
+    }
+  }
 }
 
 #[tokio::main]
@@ -44,6 +93,18 @@ async fn main() {
     }],
     code: vec![OpCode::CallNative, OpCode::Print],
   });
+
+  //   vm.queue_function(Function {
+  //     name: "Poll 3 Test".into(),
+  //     initial_stack: vec![{
+  //       fn poll_3_test() -> AsyncFnResult {
+  //         poll_n_test(3)
+  //       }
+
+  //       Object::NativeFnAsync(poll_3_test)
+  //     }],
+  //     code: vec![OpCode::CallNative, OpCode::Print],
+  //   });
 
   vm.queue_function(Function {
     name: "Async Test 2".into(),
