2dbe892b32cccdb24a96cdd2c9a0602179fa04c4
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-11-11 01:28:58 +0100

    Parser: implement a big chunk of expression parsing - no tests, so not confirmed to actually work.

--- crates/syntax/src/parser.rs
+++ crates/syntax/src/parser.rs
@@ -11,7 +11,8 @@
 use thiserror::Error;
 
 use crate::ast;
-use crate::ast::{ExprKind, Ident, Module, StmtKind, TypeKind};
+use crate::ast::expr::{BinaryOp, UnaryOp};
+use crate::ast::{expr, ExprKind, Ident, Module, StmtKind, TypeKind};
 use crate::lexer::{Lexer, Token, TokenKind, BRACKETS, PARENS};
 use crate::span::{Span, Spanned};
 
@@ -26,6 +27,7 @@ pub fn parse(source: &str) -> Result<Module<'_>, Vec<Error>> {
     lexer,
     module: Module::default(),
     errors: vec![],
+    span_offset: 0,
   }
   .parse()
 }
@@ -34,6 +36,8 @@ struct Parser<'a> {
   lexer: Lexer<'a>,
   module: Module<'a>,
   errors: Vec<Error>,
+  // If we're in a sub-lexer, all spans must be offset by this amount
+  span_offset: usize,
 }
 
 impl<'a> Parser<'a> {
@@ -152,10 +156,10 @@ impl<'a> Parser<'a> {
   }
 
   fn parse_expr_assign(&mut self) -> Result<ExprKind<'a>, Error> {
-    use ast::expr::AssignOp;
     use ast::ExprKind::{GetField, GetIndex, GetVar};
+    use expr::AssignOp;
     use TokenKind::{
-      Equal, MinusEqual, PercentEqual, PlusEqual, SlashEqual, Star2Equal, StarEqual,
+      Equal, MinusEqual, PercentEqual, PlusEqual, SlashEqual, StarEqual, StarStarEqual,
     };
 
     let mut expr = self.span(Self::parse_expr_range)?;
@@ -167,21 +171,20 @@ impl<'a> Parser<'a> {
         StarEqual => Some(AssignOp::Mul),
         SlashEqual => Some(AssignOp::Div),
         PercentEqual => Some(AssignOp::Rem),
-        Star2Equal => Some(AssignOp::Pow),
+        StarStarEqual => Some(AssignOp::Pow),
         _ => break,
       };
       self.bump(); // bump assignment operator
       let value = self.span(Self::parse_expr_range)?;
       let target_span = expr.span;
-      let span = expr.span.start..value.span.end;
       expr = Spanned::new(
+        expr.span.join(value.span),
         match expr.into_inner() {
-          GetVar(v) => ast::expr::set_var(v.name, value, op),
-          GetField(v) => ast::expr::set_field(v.target, v.name, value, op),
-          GetIndex(v) => ast::expr::set_index(v.target, v.key, value, op),
+          GetVar(v) => expr::set_var(v.name, value, op),
+          GetField(v) => expr::set_field(v.target, v.key, value, op),
+          GetIndex(v) => expr::set_index(v.target, v.key, value, op),
           _ => return Err(Error::invalid_assign(target_span)),
         },
-        span,
       );
     }
 
@@ -189,66 +192,273 @@ impl<'a> Parser<'a> {
   }
 
   fn parse_expr_range(&mut self) -> Result<ExprKind<'a>, Error> {
-    use TokenKind::RangeEx;
+    use TokenKind::Range;
 
     let left = self.span(Self::parse_expr_binary)?;
-    if self.bump_if(RangeEx) {
+    if self.bump_if(Range) {
       let right = self.span(Self::parse_expr_binary)?;
-      return Ok(ast::expr::range(left, right));
+      return Ok(expr::range(left, right));
     }
 
     Ok(left.into_inner())
   }
 
   fn parse_expr_binary(&mut self) -> Result<ExprKind<'a>, Error> {
-    todo!()
+    self.parse_expr_opt()
   }
 
   fn parse_expr_opt(&mut self) -> Result<ExprKind<'a>, Error> {
-    todo!()
+    use TokenKind::Nullish;
+
+    let mut left = self.span(Self::parse_expr_or)?;
+    while self.bump_if(Nullish) {
+      let right = self.span(Self::parse_expr_or)?;
+      left = Spanned::new(
+        left.span.join(right.span),
+        expr::binary(BinaryOp::NullOr, left, right),
+      );
+    }
+    Ok(left.into_inner())
   }
 
   fn parse_expr_or(&mut self) -> Result<ExprKind<'a>, Error> {
-    todo!()
+    use TokenKind::PipePipe;
+
+    let mut left = self.span(Self::parse_expr_and)?;
+    while self.bump_if(PipePipe) {
+      let right = self.span(Self::parse_expr_and)?;
+      left = Spanned::new(
+        left.span.join(right.span),
+        expr::binary(BinaryOp::BoolOr, left, right),
+      );
+    }
+    Ok(left.into_inner())
   }
 
   fn parse_expr_and(&mut self) -> Result<ExprKind<'a>, Error> {
-    todo!()
+    use TokenKind::AndAnd;
+
+    let mut left = self.span(Self::parse_expr_eq)?;
+    while self.bump_if(AndAnd) {
+      let right = self.span(Self::parse_expr_eq)?;
+      left = Spanned::new(
+        left.span.join(right.span),
+        expr::binary(BinaryOp::BoolAnd, left, right),
+      );
+    }
+    Ok(left.into_inner())
   }
 
   fn parse_expr_eq(&mut self) -> Result<ExprKind<'a>, Error> {
-    todo!()
+    use TokenKind::{BangEqual, EqualEqual};
+
+    let mut left = self.span(Self::parse_expr_comp)?;
+    loop {
+      let op = match self.current().kind {
+        EqualEqual => BinaryOp::Equals,
+        BangEqual => BinaryOp::NotEquals,
+        _ => break,
+      };
+      self.bump(); // bump operator
+      let right = self.span(Self::parse_expr_comp)?;
+      left = Spanned::new(left.span.join(right.span), expr::binary(op, left, right));
+    }
+    Ok(left.into_inner())
   }
 
   fn parse_expr_comp(&mut self) -> Result<ExprKind<'a>, Error> {
-    todo!()
+    use TokenKind::{Less, LessEqual, More, MoreEqual};
+
+    let mut left = self.span(Self::parse_expr_add)?;
+    loop {
+      let op = match self.current().kind {
+        Less => BinaryOp::CompareLess,
+        LessEqual => BinaryOp::CompareLessEq,
+        More => BinaryOp::CompareMore,
+        MoreEqual => BinaryOp::CompareMoreEq,
+        _ => break,
+      };
+      self.bump(); // bump operator
+      let right = self.span(Self::parse_expr_add)?;
+      left = Spanned::new(left.span.join(right.span), expr::binary(op, left, right));
+    }
+    Ok(left.into_inner())
   }
 
   fn parse_expr_add(&mut self) -> Result<ExprKind<'a>, Error> {
-    todo!()
+    use TokenKind::{Minus, Plus};
+
+    let mut left = self.span(Self::parse_expr_mul)?;
+    loop {
+      let op = match self.current().kind {
+        Plus => BinaryOp::Add,
+        Minus => BinaryOp::Subtract,
+        _ => break,
+      };
+      self.bump(); // bump operator
+      let right = self.span(Self::parse_expr_mul)?;
+      left = Spanned::new(left.span.join(right.span), expr::binary(op, left, right));
+    }
+    Ok(left.into_inner())
   }
 
   fn parse_expr_mul(&mut self) -> Result<ExprKind<'a>, Error> {
-    todo!()
+    use TokenKind::{Percent, Slash, Star};
+
+    let mut left = self.span(Self::parse_expr_pow)?;
+    loop {
+      let op = match self.current().kind {
+        Star => BinaryOp::Multiply,
+        Slash => BinaryOp::Divide,
+        Percent => BinaryOp::Remainder,
+        _ => break,
+      };
+      self.bump(); // bump operator
+      let right = self.span(Self::parse_expr_pow)?;
+      left = Spanned::new(left.span.join(right.span), expr::binary(op, left, right));
+    }
+    Ok(left.into_inner())
   }
 
   fn parse_expr_pow(&mut self) -> Result<ExprKind<'a>, Error> {
-    todo!()
+    use TokenKind::StarStar;
+
+    let mut left = self.span(Self::parse_expr_unary)?;
+    while self.bump_if(StarStar) {
+      let right = self.span(Self::parse_expr_unary)?;
+      left = Spanned::new(
+        left.span.join(right.span),
+        expr::binary(BinaryOp::Power, left, right),
+      );
+    }
+    Ok(left.into_inner())
   }
 
   fn parse_expr_unary(&mut self) -> Result<ExprKind<'a>, Error> {
-    todo!()
+    self.parse_expr_prefix()
   }
 
   fn parse_expr_prefix(&mut self) -> Result<ExprKind<'a>, Error> {
-    todo!()
-  }
+    use TokenKind::{Bang, Minus};
 
-  fn parse_expr_primary(&mut self) -> Result<ExprKind<'a>, Error> {
-    todo!()
+    let op = match self.current().kind {
+      Minus => UnaryOp::Invert,
+      Bang => UnaryOp::Negate,
+      _ => return self.parse_expr_postfix(),
+    };
+    self.bump(); // bump operator
+    let inner = self.span(Self::parse_expr_unary)?;
+    Ok(expr::unary(op, inner))
   }
 
   fn parse_expr_postfix(&mut self) -> Result<ExprKind<'a>, Error> {
+    use TokenKind::{BracketL, Comma, Dot, Optional, ParenL};
+
+    let mut expr = self.span(Self::parse_expr_primary)?;
+    loop {
+      let opt = self.bump_if(Optional);
+      match self.current().kind {
+        // call
+        ParenL => {
+          let args = self.span(|p| p.wrap_list(PARENS, Comma, |p| p.span(Self::parse_expr)))?;
+          expr = Spanned::new(
+            expr.span.join(args.span),
+            expr::call(opt, expr, args.into_inner()),
+          );
+        }
+        // index
+        BracketL => {
+          let key = self.span(|p| p.wrap(BRACKETS, |p| p.span(Self::parse_expr)))?;
+          expr = Spanned::new(
+            expr.span.join(key.span),
+            expr::get_index(opt, expr, key.into_inner()),
+          );
+        }
+        // get field or cast
+        Dot => {
+          self.bump();
+          if !self.current().is(ParenL) {
+            // get field
+            let key = self.parse_ident()?;
+            expr = Spanned::new(expr.span.join(key.span), expr::get_field(opt, expr, key));
+          } else {
+            // cast
+            let ty = self.span(|p| p.wrap(PARENS, |p| p.span(Self::parse_type)))?;
+            expr = Spanned::new(
+              expr.span.join(ty.span),
+              expr::cast(opt, expr, ty.into_inner()),
+            );
+          }
+        }
+        _ => break,
+      }
+    }
+    Ok(expr.into_inner())
+  }
+
+  fn parse_expr_primary(&mut self) -> Result<ExprKind<'a>, Error> {
+    use TokenKind::{Bool, Float, Ident, Int, Null, String};
+
+    // expr_null
+    if self.bump_if(Null) {
+      return Ok(expr::null());
+    }
+
+    // expr_bool
+    if let Bool(v) = self.current().kind {
+      self.bump();
+      return Ok(expr::bool(v));
+    }
+
+    // expr_number -> int
+    if self.bump_if(Int) {
+      let token = self.previous();
+      return Ok(expr::int(
+        token
+          .lexeme
+          .parse()
+          .map_err(|e| Error::parse_int(token.span, e))?,
+      ));
+    }
+
+    // expr_number -> float
+    if self.bump_if(Float) {
+      let token = self.previous();
+      let value = if token.lexeme.as_ref() == "inf" {
+        std::f32::INFINITY
+      } else {
+        token
+          .lexeme
+          .parse()
+          .map_err(|e| Error::parse_float(token.span, e))?
+      };
+      return Ok(expr::float(value));
+    }
+
+    // expr_string
+    if self.bump_if(String) {
+      return Ok(expr::string(self.previous().lexeme.trim_matches('"')));
+    }
+
+    // expr_array
+    // expr_tuple
+
+    // expr_var
+    if self.bump_if(Ident) {
+      let ident = Spanned::new(self.previous().span, self.previous().lexeme.clone());
+      return Ok(expr::get_var(ident));
+    }
+
+    // expr_ctrl
+    // expr_block TODO: rename to `expr_do`
+    // expr_if
+    // expr_try
+    // expr_spawn
+    // expr_lamda
+    // expr_group
+
+    // + unexpected token
+
     todo!()
   }
 
@@ -257,10 +467,10 @@ impl<'a> Parser<'a> {
   }
 
   fn parse_type_option(&mut self) -> Result<TypeKind<'a>, Error> {
-    use TokenKind::Question;
+    use TokenKind::Optional;
 
     let ty = self.span(Self::parse_type_postfix)?;
-    let ty = if self.bump_if(Question) {
+    let ty = if self.bump_if(Optional) {
       // type_optional
       ast::ty::option(ty)
     } else {
@@ -350,7 +560,7 @@ impl<'a> Parser<'a> {
     use TokenKind::Ident;
     self
       .expect(Ident)
-      .map(|token| Spanned::new(token.lexeme.clone(), token.span))
+      .map(|token| Spanned::new(token.span, token.lexeme.clone()))
   }
 }
 
@@ -492,10 +702,10 @@ impl<'a> Parser<'a> {
     &mut self,
     cons: impl FnOnce(&mut Self) -> Result<T, Error>,
   ) -> Result<Spanned<T>, Error> {
-    let start = self.current().span.start;
+    let start = self.current().span;
     cons(self).map(|value| {
-      let end = self.previous().span.end;
-      Spanned::new(value, start..end)
+      let end = self.previous().span;
+      Spanned::new(start.join(end), value)
     })
   }
 
@@ -521,6 +731,66 @@ impl<'a> Parser<'a> {
   }
 }
 
+// Adapted from https://docs.rs/snailquote/0.3.0/x86_64-pc-windows-msvc/src/snailquote/lib.rs.html.
+/// Unescapes the given string in-place. Returns `None` if the string contains
+/// an invalid escape sequence.
+pub(crate) fn unescape_in_place(s: &mut String) -> Option<()> {
+  let mut out = String::with_capacity(s.len());
+  let mut chars = s.chars();
+  while let Some(ch) = chars.next() {
+    if ch == '\\' {
+      if let Some(next) = chars.next() {
+        let escape = match next {
+          'a' => Some('\u{07}'),
+          'b' => Some('\u{08}'),
+          'v' => Some('\u{0B}'),
+          'f' => Some('\u{0C}'),
+          'n' => Some('\n'),
+          'r' => Some('\r'),
+          't' => Some('\t'),
+          '\'' => Some('\''),
+          '"' => Some('"'),
+          'e' | 'E' => Some('\u{1B}'),
+          'u' => Some(parse_unicode(&mut chars)?),
+          _ => None,
+        };
+        match escape {
+          Some(esc) => {
+            out.push(esc);
+          }
+          None => {
+            out.push(ch);
+            out.push(next);
+          }
+        }
+      }
+    } else {
+      out.push(ch);
+    }
+  }
+  *s = out;
+  Some(())
+}
+
+// Adapted from https://docs.rs/snailquote/0.3.0/x86_64-pc-windows-msvc/src/snailquote/lib.rs.html.
+fn parse_unicode<I>(chars: &mut I) -> Option<char>
+where
+  I: Iterator<Item = char>,
+{
+  match chars.next() {
+    Some('{') => {}
+    _ => {
+      return None;
+    }
+  }
+
+  let unicode_seq: String = chars.take_while(|&c| c != '}').collect();
+
+  u32::from_str_radix(&unicode_seq, 16)
+    .ok()
+    .and_then(char::from_u32)
+}
+
 #[derive(Clone, Debug, Error)]
 pub enum Error {
   #[error("invalid character sequence `{0}` at {1}")]
@@ -533,6 +803,12 @@ pub enum Error {
   Unexpected(TokenKind, Span),
   #[error("invalid assignment target at {0}")]
   InvalidAssign(Span),
+  #[error("string at {0} contains an invalid escape sequence")]
+  InvalidEscapeSequence(Span),
+  #[error("failed to parse int at {0}: {1}")]
+  ParseInt(Span, #[source] std::num::ParseIntError),
+  #[error("failed to parse float at {0}: {1}")]
+  ParseFloat(Span, #[source] std::num::ParseFloatError),
 }
 
 impl Error {
@@ -553,7 +829,19 @@ impl Error {
   }
 
   fn invalid_assign(span: Span) -> Self {
-    Error::invalid_assign(span)
+    Error::InvalidAssign(span)
+  }
+
+  fn invalid_escape_sequence(span: Span) -> Self {
+    Error::InvalidEscapeSequence(span)
+  }
+
+  fn parse_int(span: Span, e: std::num::ParseIntError) -> Self {
+    Error::ParseInt(span, e)
+  }
+
+  fn parse_float(span: Span, e: std::num::ParseFloatError) -> Self {
+    Error::ParseFloat(span, e)
   }
 }
 
